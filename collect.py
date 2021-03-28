1db3cd5a9b88b2e5516321b2c3f52bb6f0fdf92b
aatherton <aatherton1990@live.com>
2021-03-28 12:46:28 -0500

    Update collect.py

--- collect.py
+++ collect.py
@@ -6,26 +6,29 @@ import requests
 ### declare globals ###
 
 HEADERS = {"accept": "application/json", "Platform": "pc"}
-BASE = "https://api.warframe.market/v1/items/"
+BASE = "https://api.warframe.market/v1/items"
 
 ### define functions ###
 
 ### main logic ###
 
 # query to get list of items
-items = requests.get(BASE, HEADERS).json()["payload"]["items"]
+items = requests.get(BASE, headers=HEADERS).json()["payload"]["items"]
 # declare result as empty dict
 result = {}
 for each in items:
   # query to get orders for that item
-  query = requests.get(f"{BASE}{each.url_name}/orders", HEADERS).json()["payload"]["orders"]
+  query = requests.get(f"{BASE}/{each['url_name']}/orders", headers=HEADERS).json()["payload"]["orders"]
   # select platinum where order_type = "buy"
   pipe = []
   for other in query:
-    if other.order_type == "buy":
-      pipe.append(other.platinum)
+    if other['order_type'] == "buy":
+      pipe.append(other['platinum'])
   # get mode
-  pipe = statistics.mode(pipe)
+  try:
+    pipe = statistics.mode(pipe)
+  except:
+    pipe = 0
   # add {each.url_name: mode} to result
   result[each.url_name] = pipe
 return result
